d637fdf7625e3577991f2d55e1017fcbb0f54988
wademacintosh <wade.macintosh@edmonton.ca>
2026-04-26 03:11:12 -0600

    Remove duplicate styles.css import
    Removed duplicate import of styles.css.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import './styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles.css'
 
 // We removed the TanStack Router logic to stop the Vercel 404 errors.
 // This now uses standard React loading.
