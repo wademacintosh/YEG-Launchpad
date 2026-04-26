32bb34e09929a1ae363cc4e57c78c4cd9c5402e8
wademacintosh <wade.macintosh@edmonton.ca>
2026-04-26 01:39:13 -0600

    Remove AppRouter component and its imports

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,17 +1,1 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RootLayout from "./pages/RootLayout";
-import HomePage from "./pages/HomePage";
-import ThemePage from "./pages/ThemePage";
 
-export function AppRouter() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<RootLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/theme/:slug" element={<ThemePage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
