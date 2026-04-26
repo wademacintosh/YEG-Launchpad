4c8574c2a88213f327f5009107a914afb4b5c73a
wademacintosh <wade.macintosh@edmonton.ca>
2026-04-26 03:13:11 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Ensure this is here!
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 8080,
-  },
-});
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
