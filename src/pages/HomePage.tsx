70a7fc5ec8225a80d92464f1dfedf7494b123d07
wademacintosh <wade.macintosh@edmonton.ca>
2026-04-26 01:54:56 -0600

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,11 @@
-import { Navbar } from "@/components/Navbar";
-import { Hero } from "@/components/Hero";
-import { BridgeDivider } from "@/components/BridgeDivider";
-import { StyleGallery } from "@/components/StyleGallery";
-import { Footer } from "@/components/Footer";
-import { Toaster } from "@/components/ui/sonner";
+import React from 'react';
+import StyleGallery from '../components/StyleGallery';
 
 export default function HomePage() {
   return (
-    <div className="flex min-h-screen flex-col bg-background">
-      <Navbar />
-      <main className="flex-1">
-        <Hero />
-        <BridgeDivider />
-        <StyleGallery />
-      </main>
-      <Footer />
-      <Toaster richColors position="top-right" />
+    <div className="min-h-screen bg-white">
+      {/* Add your Navbar or Hero here if you want */}
+      <StyleGallery />
     </div>
   );
 }
