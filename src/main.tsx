81190fcd98082325e810776932a33f8ab0a55b76
wademacintosh <wade.macintosh@edmonton.ca>
2026-04-28 01:11:02 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles.css'
+import './style.css' // FIXED: Changed from styles.css to style.css
 
 /**
  * CONFIGURATION: Root Element ID
