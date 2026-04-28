0cc4b815f7725d97b80ad249ae44fd74fec6aeb8
wademacintosh <wade.macintosh@edmonton.ca>
2026-04-27 23:49:46 -0600

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,37 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       keyframes: {
         'fade-up': {
-          '0%': { 
-            opacity: '0', 
-            transform: 'translateY(20px)' 
-          },
-          '100%': { 
-            opacity: '1', 
-            transform: 'translateY(0)' 
-          },
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
         },
         'fade-in': {
-          '0%': { 
-            opacity: '0' 
-          },
-          '100%': { 
-            opacity: '1' 
-          },
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
         },
         'float': {
-          '0%, 100%': { 
-            transform: 'translateY(0)' 
-          },
-          '50%': { 
-            transform: 'translateY(-10px)' 
-          },
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
         }
       },
       animation: {
@@ -42,4 +25,4 @@ export default {
     },
   },
   plugins: [],
-}
+};
